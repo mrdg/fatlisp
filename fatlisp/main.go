e122ba1291b00776610418eb4c507a0acf64259c
Menno de Gier <mrdegier@gmail.com>
2013-10-01 08:01:45 +0200

    accept input file as command line param

--- fatlisp/main.go
+++ fatlisp/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mrdg/fatlisp"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	src, err := ioutil.ReadFile("test.clj")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Fprintf(os.Stderr, "usage: %s <file>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	file := flag.Arg(0)
+
+	src, err := ioutil.ReadFile(file)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 	} else {
-		tree := fatlisp.NewTree("test.clj", string(src))
+		tree := fatlisp.NewTree(file, string(src))
 		nodes := tree.Parse()
 		fatlisp.Eval(nodes)
+
 	}
 }
