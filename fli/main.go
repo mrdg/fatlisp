0fd6817bc73798033ce3bcba05f65ce73b5ad5a6
Menno de Gier <mrdegier@gmail.com>
2013-10-18 13:01:00 +0200

    Rename executable to 'fli'

--- fli/main.go
+++ fli/main.go
@@ -14,23 +14,25 @@ func main() {
 	if flag.NArg() < 1 {
 		fmt.Fprintf(os.Stderr, "usage: %s <file>\n", os.Args[0])
 		flag.PrintDefaults()
-		os.Exit(2)
+		os.Exit(1)
 	}
 	file := flag.Arg(0)
 
 	src, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(2)
+		os.Exit(1)
 	}
 
 	tree, err := fatlisp.Parse(file, string(src))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
 	_, err = fatlisp.Eval(tree)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
---
+++ fli/test.clj
@@ -0,0 +1,4 @@
+(def square
+  (fn (x) (+ 1 x)))
+
+(puts "wut" (bar 4))
